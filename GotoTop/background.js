0105ca97e2f0da7ac50616a3705f3002f07953af
gyu0409 <gyu0409@gmail.com>
2023-05-09 15:59:44 +0900

    위 아래 기능 추가

--- GotoTop/background.js
+++ GotoTop/background.js
@@ -1,6 +1,27 @@
-  
-  // 상단 이동 함수
-  async function goTop() {
+
+  // Alt + Q 로 확장프로그램 팝업 실행시
+  // window.onload = move();
+
+  // 버튼 단축키
+  chrome.commands.onCommand.addListener(function(command) {
+    console.log(command)
+    if (command === 'up') { // 등록한 단축키의 이름
+      console.log("up");
+      moveUp();
+      }
+    if(command === 'down'){
+      console.log("dd");
+      moveDown();
+    }
+    });
+
+  // 버튼 클릭 이벤트 핸들러 등록
+  document.getElementById('upBtn').addEventListener('click', moveUp());
+  document.getElementById('downBtn').addEventListener('click', moveDown());
+
+
+   // 이동 함수
+   async function moveUp() {
 
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       const tabId = tabs[0].id;
@@ -10,26 +31,25 @@
       chrome.scripting.executeScript({
       target: {tabId: tabId},
       func: () => {
-        window.scrollTo({ left: 0, top: 0, behavior: "smooth" })
+          window.scrollTo({ left: 0, top: 0, behavior: "smooth" })
       }
       })
-
     });
-
   }
 
-  
-  // Alt + Q 로 확장프로그램 팝업 실행시
-  window.onload = goTop();
+  // 이동 함수
+  async function moveDown() {
 
-  // 버튼 단축키
-  chrome.commands.onCommand.addListener(function(command) {
-    if (command === 'my_command') { // 등록한 단축키의 이름
-      goTop();
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      const tabId = tabs[0].id;
+
+      console.log(tabId);
+    
+      chrome.scripting.executeScript({
+      target: {tabId: tabId},
+      func: () => {
+          window.scrollTo({ left: 0, top: 999999, behavior: "smooth" })
       }
+      })
     });
-
-  // 버튼 클릭 이벤트 핸들러 등록
-  document.getElementById('topBtn').addEventListener('click', goTop);
-
-  // it will work?
+  }
